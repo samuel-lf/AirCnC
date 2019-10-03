3e3daff951ab4d4b99e0a5107273a241d6898650
Samuel Leonel <samueleonelfreitas@hotmail.com>
2019-10-02 21:56:51 -0300

    Ajustes de CORS e exibição de imaagem

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
@@ -8,8 +10,9 @@ mongoose.connect('mongodb+srv://samuel:1234@aircnc-fmbdx.mongodb.net/aircnc?retr
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
 app.listen(3000);
